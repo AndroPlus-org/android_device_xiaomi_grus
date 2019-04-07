f5de4e4599c1aed188ee1c6e16c6a0478ec4fdef
AndroPlus <mail@andro.plus>
2019-04-07 17:02:53 +0900

    Rebrand to grus

--- omni_grus.mk
+++ omni_grus.mk
@@ -2,16 +2,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_xmsirius
-PRODUCT_DEVICE := xmsirius
+PRODUCT_NAME := omni_grus
+PRODUCT_DEVICE := grus
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 8 SE
+PRODUCT_MODEL := MI 9 SE
 PRODUCT_MANUFACTURER := Xiaomi
 
 # If needed to overide these props
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="Xiaomi/sirius/sirius:8.1.0/OPM1.171019.019/V9.5.9.0.OEBCNFA:user/release-keys" \
-    PRIVATE_BUILD_DESC="sirius-user 8.1.0 OPM1.171019.019 V9.5.9.0.OEBCNFA release-keys"
+    BUILD_FINGERPRINT="Xiaomi/grus/grus:9/PKQ1.181121.001/V10.2.5.0.PFBCNXM:user/release-keys" \
+    PRIVATE_BUILD_DESC="grus-user 9 PKQ1.181121.001 V10.2.5.0.PFBCNXM release-keys"
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.treble.enabled=true \
@@ -21,7 +21,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.service.debuggable=1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=sirius \
-    PRODUCT_NAME=sirius \
-    TARGET_VENDOR_PRODUCT_NAME=sirius \
-    TARGET_VENDOR_DEVICE_NAME=sirius
+    TARGET_DEVICE=grus \
+    PRODUCT_NAME=grus \
+    TARGET_VENDOR_PRODUCT_NAME=grus \
+    TARGET_VENDOR_DEVICE_NAME=grus
